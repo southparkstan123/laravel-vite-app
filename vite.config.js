5ca15f62482b6d05af9350e6d7ea20157ed43f4e
southparkstan123 <southparkstan123@gmail.com>
2023-08-12 12:19:00 +0800

    Use Vite env variables in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,36 @@
-const host = '192.168.56.56'; 
-import { defineConfig } from 'vite';
+
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.ts'],
-            refresh: true
-        }),
-        vue()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('resources/ts', import.meta.url))
+export default defineConfig(({ mode }) => {
+    process.env = { ...loadEnv(mode, process.cwd()) };
+
+    const host = process.env.VITE_HOST;
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/ts/app.ts'],
+                refresh: true
+            }),
+            vue()
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('resources/ts', import.meta.url))
+            },
         },
-    },
-    server: {
-        host,
-        hmr: {
+        server: {
             host,
-        },
-        watch: {
-            usePolling: true
+            hmr: {
+                host,
+            },
+            watch: {
+                usePolling: true
+            }
         }
-    },
+    }
+
 });
