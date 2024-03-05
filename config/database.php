15971a745301c4e2b3d185c85c8263bd97cf34ce
southparkstan123 <southparkstan123@gmail.com>
2024-03-05 10:15:15 +0800

    Modify the config for MongoDB

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
 
         // 'mongodb' => [ 
         //     'driver' => 'mongodb', 
-        //     'host' => env('MONGO_DB_HOST', '127.0.0.1'), 
+        //     'host' => env('MONGO_DB_HOST', 'mongo'), 
         //     'port' => env('MONGO_DB_PORT', '27017'), 
         //     'database' => env('MONGO_DB_DATABASE') , 
         //     'username' => env('MONGO_DB_USERNAME'), 
